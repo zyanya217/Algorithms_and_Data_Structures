555ea1920bdfef3b197a3685130d5e39d88b812a
zyanya <83296460+zyanya217@users.noreply.github.com>
2023-10-27 16:37:22 +0800

    Update Primes.cpp

--- Algorithms/Primes.cpp
+++ Algorithms/Primes.cpp
@@ -3,29 +3,33 @@ The user inputs a value and the program outputs its prime number.
  */
 
 #include <iostream>
-#include <cmath>
 using namespace std;
 
-bool isPrime(int x);
+const int MAX_LEN = 10000;
+
+void ruleOutPrime(int x,bool isPrime[], int n);
+
 int main()
 {
 	int n = 0;
-	cin >> n;
+	cin >> n; //must <10000
+	bool isPrime[MAX_LEN] = {0};	
+	for(int i = 0; i < n; i++)
+		isPrime[i] = true;
 		
-	for(int i=2 ; i<=n ; i++)
+	for(int i = 2; i <= n; i++)
 	{
-		if(isPrime(i) == true)
-		cout << i<< " ";
+		if(isPrime[i] == true)
+		{
+			cout << i<< " ";
+			ruleOutPrime(i, isPrime, n);	
+		}
 	}
 	return 0;
 }
 
-bool isPrime(int x)
+void ruleOutPrime(int x,bool isPrime[], int n)
 {
-	for(int i=2; i * i <= x;i++) 
-	{
-		if(x % i ==0)
-			return false;
-	}
-	return true;
+	for(int i = 1; x * i < n; i++)
+	isPrime[x * i] = false ;
 }
