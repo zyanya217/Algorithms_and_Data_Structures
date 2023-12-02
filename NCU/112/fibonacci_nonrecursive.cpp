05c4a2ec794f5e76af9ce0fc4016ac75a38f8334
zyanya <83296460+zyanya217@users.noreply.github.com>
2023-12-02 16:10:19 +0800

    Update fibonacci_nonrecursive.cpp

--- NCU/112/fibonacci_nonrecursive.cpp
+++ NCU/112/fibonacci_nonrecursive.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+using namespace std;
 
 void printFib(int limit) {
     int fib1 = 0, fib2 = 1, nextFib;
     while (fib1 < limit) {
-        std::cout << fib1 << " ";
+        cout << fib1 << " ";
         nextFib = fib1 + fib2;
         fib1 = fib2;
         fib2 = nextFib;
