db4cad3465ec831ba52c78c4ad89fc3e067e18c4
zyanya <83296460+zyanya217@users.noreply.github.com>
2023-12-29 15:55:40 +0800

    Update two_8sidedDice.cpp

--- NCKU/108/two_8sidedDice.cpp
+++ NCKU/108/two_8sidedDice.cpp
@@ -5,20 +5,22 @@
 using namespace std;
 
 int main() {
-    const int sides = 8;
-    int results[sides + 1][sides + 1] = {}; // 陣列大小為 9x9，從1開始
+    const int size = 8;
+    int freq[size + 1][size + 1] = {}; 
+    
+    srand((time(0))); // 使用系统時間設置隨機數生成器的種子
 
     // 擲骰子並記錄次數
     for (int roll = 1; roll <= 100 ; roll++) {
-        int dice1 = 1 + rand() % sides;
-        int dice2 = 1 + rand() % sides;
-        results[dice1][dice2]++;
+        int dice1 = 1 + rand() % size;
+        int dice2 = 1 + rand() % size;
+        freq[dice1][dice2]++;
     }
 
     // 顯示結果
-    for (int face1 = 1; face1 <= sides; face1++) {
-        for (int face2 = 1; face2 <= sides; face2++) {
-            cout << setw(4) << results[face1][face2];
+    for (int face1 = 1; face1 <= size; face1++) {
+        for (int face2 = 1; face2 <= size; face2++) {
+            cout << setw(4) << freq[face1][face2];
         }
         cout << endl;
     }
