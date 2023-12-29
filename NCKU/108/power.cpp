f69f73c51bc01548311f7a9f1f257d41659bf26c
zyanya <83296460+zyanya217@users.noreply.github.com>
2023-12-29 17:38:04 +0800

    Update power.cpp

--- NCKU/108/power.cpp
+++ NCKU/108/power.cpp
@@ -3,15 +3,14 @@ using namespace std;
 
 // 定義計算 x^n 的快速指數算法
 int myPow(int x, int n) {
-    int result = 1;
-  
-    while (n) {
+        // 如果 n 的二進制表示的最低位為 1
         if (n % 2 == 1) {
-            result *= x;
+            result *= x; // 將結果乘以當前的底數 x
         }
-        x *= x;
-        n /= 2;
+        x *= x;  // 將底數 x 自乘
+        n /= 2; // 將指數 n 右移一位
     }
+    // 返回計算結果
     return result;
 }
 
