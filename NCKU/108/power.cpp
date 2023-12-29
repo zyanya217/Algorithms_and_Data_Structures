5cbdea905947c6ae1317acb6d3e47c09020f4063
zyanya <83296460+zyanya217@users.noreply.github.com>
2023-12-29 17:38:52 +0800

    Update power.cpp

--- NCKU/108/power.cpp
+++ NCKU/108/power.cpp
@@ -18,7 +18,7 @@ int main() {
     
     int x, n;
     cout << "Enter the base and exponent (x^n): ";
-    cin >> x >> n; // 接收用戶輸入的底數 x 和指數 n
+    cin >> x >> n; // 接收使用者輸入的底數 x 和指數 n
 
     cout << x << "^" << n << " = " << myPow(x, n) << endl; // 計算並輸出結果
 
