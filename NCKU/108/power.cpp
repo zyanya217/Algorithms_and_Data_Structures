41558fa5cd67b463381c504cff9470ff6f0f6615
zyanya <83296460+zyanya217@users.noreply.github.com>
2023-12-29 17:33:37 +0800

    Update power.cpp

--- NCKU/108/power.cpp
+++ NCKU/108/power.cpp
@@ -1,1 +1,27 @@
+#include <iostream>
+using namespace std;
 
+// 定義計算 x^n 的快速指數算法
+int myPow(int x, int n) {
+    int result = 1;
+  
+    while (n) {
+        if (n % 2 == 1) {
+            result *= x;
+        }
+        x *= x;
+        n /= 2;
+    }
+    return result;
+}
+
+int main() {
+    
+    int x, n;
+    cout << "Enter the base and exponent (x^n): ";
+    cin >> x >> n; // 接收用戶輸入的底數 x 和指數 n
+
+    cout << x << "^" << n << " = " << myPow(x, n) << endl; // 計算並輸出結果
+
+    return 0;
+}
