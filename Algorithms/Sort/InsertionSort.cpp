cb0fed5b181aecc6af94475f1d7196ef9f6f2f31
zyanya <83296460+zyanya217@users.noreply.github.com>
2024-01-31 14:13:08 +0800

    Update InsertionSort.cpp

--- Algorithms/Sort/InsertionSort.cpp
+++ Algorithms/Sort/InsertionSort.cpp
@@ -1,3 +1,31 @@
 #include <iostream>
 using namespace std;
 
+typedef int index;
+typedef int keytype; 
+
+void InsertionSort(int n, keytype S[]) {
+    index i, j;
+    keytype x;
+
+    for (i = 1; i <= n; i++) {
+        x = S[i];
+        j = i - 1;
+        while (j >= 0 && S[j] > x) {
+            S[j + 1] = S[j];
+            j--;
+        }
+        S[j + 1] = x;
+    }
+}
+
+int main() {
+    keytype arr[] = {5, 2, 9, 1, 5, 6};
+    index size = sizeof(arr) / sizeof(arr[0]);
+    InsertionSort(size - 1, arr);
+    cout << "排序後的陣列: ";
+    for (index i = 0; i < size; ++i) {
+        cout << arr[i] << " ";
+    }
+    return 0;
+}
