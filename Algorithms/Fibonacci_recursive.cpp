8ea1463a4b61b25bfbcf55a3842697a56d3dffa5
zyanya <83296460+zyanya217@users.noreply.github.com>
2024-02-03 15:03:40 +0800

    Update Fibonacci_recursive.cpp

--- Algorithms/Fibonacci_recursive.cpp
+++ Algorithms/Fibonacci_recursive.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
+using namespace std;
 
+int fib(int n)
+{
+  if(n == 1 || n == 2)
+    return 1;
+  else
+    return (fib(n-1) + fib(n-2));
+}
+
+int main() {
+  int n = 8 ;
+  cout << "費波那契數列的第 " << n << " 項為：" << fib(n) << endl;
+  return 0;
+}
